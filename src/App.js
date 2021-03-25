14b079df3cb8ab1cd10f1a58a6265f073b8491af
Ozioma Uzoegwu <ouzoegwu@amazon.com>
2021-03-25 14:39:59 +0000

    add new line

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React The Amplify Way
         </a>
       </header>
     </div>
